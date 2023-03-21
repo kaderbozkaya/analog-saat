7f5d2421eb079463a79ae781698cb6b4faa0269d
Kader Bozkaya <kaderbozkaya705@gmail.com>
2023-03-21 20:34:51 +0300

    yeni

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const sec=document.querySelector(".secs");
 const min=document.querySelector(".mins");
 const hours=document.querySelector(".hours");
-function tiktak(){
-    let second=new Date().getSeconds();
+function donme(){
+    let second=new Date().getSeconds(); /*yerelden dk sn vs çekiyoruz*/
     let minute=new Date().getMinutes();
     let hour=new Date().getHours();
-   sec.style.transform=`rotate(${180+(second*6)}deg)`
+   sec.style.transform=`rotate(${180+(second*6)}deg)` /*iç açıları top 360 derece. 360/60=6*/
    min.style.transform=`rotate(${180+(minute*6)}deg)`
-   hours.style.transform=`rotate(${180+(hour*30)}deg)` //otuz derece her saat aralığı
+   hours.style.transform=`rotate(${180+(hour*30)}deg)` /*360/12=30 */
     console.log({second,minute,hour})
 
 
 }
-setInterval(tiktak,1000);
+setInterval(donme,1000);
